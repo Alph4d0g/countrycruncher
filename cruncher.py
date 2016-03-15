ec6f5de1227ecbfbd9244a61d031db45807d00aa
Alph4d0g <alp4d0g007@googlemail.com>
2016-03-15 14:31:04 +0100

    modified cruncher, deleted testfile

--- cruncher.py
+++ cruncher.py
@@ -1,33 +1,31 @@
-import sys
-import re
-glob_SingleLine = None
-index = None
-
-testvalue = "123456789"
+import argparse
 
 
-def checkcountry(string):
-    if re.match("^1.*", string):
-        cc = "1"
-        nsn = string[1:]
-        return 
-    elif string.startswith("49"):
-        print "Country is GERMANY"
-        cc = "49"
-        nsn = string [2:]
-        print nsn
-
-checkcountry(testvalue)
-
-with open('testfile.csv', 'r') as original:
-    for index in original:
-        if not index.isspace():
-            if index.startswith("00"):
-                index = index[2:]
-                checkcountry(index)
-            else:
-                checkcountry(index)
+parser = argparse.ArgumentParser()
+parser.add_argument("inputfile", help="Place you source file here")
+parser.add_argument("outputfile", help="Place you destination file here")
+args = parser.parse_args()
 
 
 
+
+print "Begin"
+with open('countrylist.csv', 'r') as countries:
+    print "Countries imported"
+    ccs = countries.read().splitlines()
+    print ccs[1]
+
+with open(args.inputfile, 'r') as original:
+    with open(args.outputfile, 'w') as output:
+
+        print "testfile opened"
+        for number in original:
+            if not number.isspace():
+                if number.startswith("00"):
+                    number = number[2:]
+                    nsn = number[len(filter(number.startswith,tuple(ccs+['']))[0]):]
+                    cc = number[:-len(nsn)]
+                    output.write(cc+","+nsn)
+
+print (output.closed)
 print (original.closed)
