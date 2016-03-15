ecf936ad9801d9262c7ee428c3eac1a3166fc57f
Alph4d0g <alp4d0g007@googlemail.com>
2016-03-15 14:32:16 +0100

    made cruncher look nice

--- cruncher.py
+++ cruncher.py
@@ -1,24 +1,14 @@
 import argparse
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("inputfile", help="Place you source file here")
 parser.add_argument("outputfile", help="Place you destination file here")
 args = parser.parse_args()
 
-
-
-
-print "Begin"
 with open('countrylist.csv', 'r') as countries:
-    print "Countries imported"
     ccs = countries.read().splitlines()
-    print ccs[1]
-
 with open(args.inputfile, 'r') as original:
     with open(args.outputfile, 'w') as output:
-
-        print "testfile opened"
         for number in original:
             if not number.isspace():
                 if number.startswith("00"):
@@ -26,6 +16,3 @@ with open(args.inputfile, 'r') as original:
                     nsn = number[len(filter(number.startswith,tuple(ccs+['']))[0]):]
                     cc = number[:-len(nsn)]
                     output.write(cc+","+nsn)
-
-print (output.closed)
-print (original.closed)
